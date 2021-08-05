b970860d7616aa4a284811959f0e7fa73f574aef
Aryan Raj <aryanraj844101@gmail.com>
2021-08-05 11:01:22 +0530

    Updates

---
+++ precache-manifest.e96fd8b41f36c0ef13a229b715d11587.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed17ad81a6fd9bf6be2606c914da6eeb",
+    "url": "/pokemon_api_-website/index.html"
+  },
+  {
+    "revision": "20af76088aad8bebb68a",
+    "url": "/pokemon_api_-website/static/css/main.47f666f0.chunk.css"
+  },
+  {
+    "revision": "87c3aeaa0f6960155e16",
+    "url": "/pokemon_api_-website/static/js/2.d09f8bdb.chunk.js"
+  },
+  {
+    "revision": "20af76088aad8bebb68a",
+    "url": "/pokemon_api_-website/static/js/main.5a70cc73.chunk.js"
+  },
+  {
+    "revision": "6f353310a9ffc7e44da9",
+    "url": "/pokemon_api_-website/static/js/runtime~main.e78fa492.js"
+  }
+]);
